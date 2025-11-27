56960a12fb8b7b707b58d043bfd098c6cde2562f
Hariharan.A <hariharansuthan05@gmail.com>
2025-11-27 21:53:22 +0530

    Update oauth-callback.js

--- api/auth/oauth-callback.js
+++ api/auth/oauth-callback.js
@@ -11,10 +11,6 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed. Use POST." });
-  }
-
   try {
     const { code, clientId, clientSecret, redirectUri } = req.body;
 
